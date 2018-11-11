86157740cef593a709a15a7272134ceb650f4073
Aaron Hornby (Desktop) <aaron.hornby@outlook.com>
2018-11-11 13:41:18 -0700

    + finished stage 1,2,3 of parser. stage 4 TODO + .gitignore

--- src/Input.java
+++ src/Input.java
@@ -82,13 +82,23 @@
     - is LEC01, LEC0 1,  possible?
     - do we need a blank line before next keyword like 449?
 
+    ANSWERS:
+    - name can't be a blank line or a keyword
+    - he expects the parsing to go character by character L->R (NL understanding)
+    - this assignment seems to be less focused on the parser, and more on the algorithm (there isnt special error messages), can just have 1 general error msg
+
 */
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
 
     // fields...
+
+	public String name;
+
 
     // figure out best data structures to store parsed data in here...
 
@@ -108,6 +118,8 @@ public class Input {
     // 10 keynames to find...
     private boolean nameKeyFound, courseSlotsKeyFound, labSlotsKeyFound, coursesKeyFound, labsKeyFound, notCompatibleKeyFound, unwantedKeyFound, preferencesKeyFound, pairKeyFound, partialAssignmentsKeyFound;
 
+	//private boolean specialErrorOccurred = false; 
+
 
     // methods...
 
@@ -116,58 +128,322 @@ public class Input {
     }
 
 
-
+    // return T or F (was parsing error-free?)
     public boolean parseFile(String filename) throws Exception {
+
+        List<String> allNonBlankTrimmedLines = new ArrayList<String>();
 
 		FileReader fr = null;
 		BufferedReader br = null; 
         try {
 			fr = new FileReader(filename);
-			br = new BufferedReader(fr); 
-			String line; // go through line by line...
+            br = new BufferedReader(fr); 
+            
+            // 1. read entire file into an arraylist that stores each NON-BLANK TRIMMED line as an element
+
+			String line; // read 1 line at a time into arraylist
 			
-			while((line = br.readLine()) != null) {
-				line = line.trim(); // remove leading and trailing whitespace...
-				//previousLine = currentLine;
-				//currentLine = line; 
-				if (line.isEmpty()) { // if trimmed line is blank... 
-					continue;
+            while ((line = br.readLine()) != null) { // read until hitting EOF...
+                
+                line = line.trim(); // remove leading and trailing whitespace...
+                
+                if (line.isEmpty()) { // if trimmed line is blank... 
+					continue; // don't add to list
 				}
-				//boolean keyFound = checkForKey(line, br, true); 
-				//if (!keyFound) { // found a value instead for the first (non-empty) line in file
-				//	Output.msg("Error while parsing input file"); // means that a header is missing at top
-				//	specialErrorOccurred = true; 
-				//}
-				//if (specialErrorOccurred)
-				//	break; 
+
+                allNonBlankTrimmedLines.add(line); // append to end of list
             }
+
         } catch (FileNotFoundException e) {
             System.out.println("Error: Input.java | The specified input file was not found.");
-            //specialErrorOccurred = true;
         } catch (IOException e) {
             System.out.println("Error: Input.java | Failed to read line properly.");
-            //specialErrorOccurred = true;
         } finally {
             if (fr != null) {
                 fr.close(); // also closes br for us
             }	
         }
 
-        //if (specialErrorOccurred) // already took care of a specific error message
-        //	return false; 
-        
 
-        // parsing was successful if no errors occurred
-		if (nameKeyFound && courseSlotsKeyFound && labSlotsKeyFound && coursesKeyFound && labsKeyFound && notCompatibleKeyFound && unwantedKeyFound && preferencesKeyFound && pairKeyFound && partialAssignmentsKeyFound) {
-			return true; // if all keywords were found
-        } 
-        else {
-			// if any keyword wasn't found...
-			//Output.msg("Error while parsing input file"); // missing a key header
-			return false;
+
+        int nameKeyIndex, courseSlotsKeyIndex, labSlotsKeyIndex, coursesKeyIndex, labsKeyIndex, notCompatibleKeyIndex, unwantedKeyIndex, preferencesKeyIndex, pairKeyIndex, partialAssignmentsKeyIndex;
+        nameKeyIndex = courseSlotsKeyIndex = labSlotsKeyIndex = coursesKeyIndex = labsKeyIndex = notCompatibleKeyIndex = unwantedKeyIndex = preferencesKeyIndex = pairKeyIndex = partialAssignmentsKeyIndex = -1; // init to an invalid index
+
+        // 2. check to make sure all keywords are in arraylist EXACTLY ONCE EACH (and if so, then we know they are in the correct order, by default)
+
+        for (int i = 0; i < allNonBlankTrimmedLines.size(); i++) {
+
+            String str = allNonBlankTrimmedLines.get(i); 
+
+            switch(str) {
+                case "Name:":
+                    if (!nameKeyFound) { // if this is the first time finding this key...
+                        nameKeyFound = true; // flag that we found it
+                        nameKeyIndex = i;
+                    } 
+                    else { // if this key was already found, we have a duplicate and thats an error...
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Course slots:":
+                    if (!courseSlotsKeyFound) {
+                        courseSlotsKeyFound = true;
+                        courseSlotsKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Lab slots:":
+                    if (!labSlotsKeyFound) {
+                        labSlotsKeyFound = true;
+                        labSlotsKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Courses:":
+                    if (!coursesKeyFound) {
+                        coursesKeyFound = true;
+                        coursesKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Labs:":
+                    if (!labsKeyFound) {
+                        labsKeyFound = true;
+                        labsKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Not compatible:":
+                    if (!notCompatibleKeyFound) {
+                        notCompatibleKeyFound = true;
+                        notCompatibleKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Unwanted:":
+                    if (!unwantedKeyFound) {
+                        unwantedKeyFound = true;
+                        unwantedKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Preferences:":
+                    if (!preferencesKeyFound) {
+                        preferencesKeyFound = true;
+                        preferencesKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Pair:":
+                    if (!pairKeyFound) {
+                        pairKeyFound = true;
+                        pairKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+                case "Partial assignments:":
+                    if (!partialAssignmentsKeyFound) {
+                        partialAssignmentsKeyFound = true;
+                        partialAssignmentsKeyIndex = i;
+                    }
+                    else {
+                        System.out.println("Error: Duplicate keyword found in input file"); // maybe change this to a general error msg later? or write this in output file?
+                        return false; // stop parsing
+                    }
+                    break;
+            }
+
         }
-        
-	}	
+
+        // now we know we dont have a duplicate keyword.
+        // make sure we don't have a missing keywrord...
+        if (!nameKeyFound || !courseSlotsKeyFound || !labSlotsKeyFound || !coursesKeyFound || !labsKeyFound || !notCompatibleKeyFound || !unwantedKeyFound || !preferencesKeyFound || !pairKeyFound || !partialAssignmentsKeyFound) {
+            System.out.println("Error: Input file is missing a keyword.");
+            return false; // stop parsing
+        }
+
+        // get here if all keywords were found EXACTLY ONCE
+        // by the assignment definition, we KNOW that the input file has the keywords in the proper order
+
+
+
+        // 3. split arraylist into 10 sublists (tables) for each keyword area
+
+
+        // first make sure the input file didn't have any garbage non-blank lines before "Name:"
+
+        if (nameKeyIndex != 0) {
+            System.out.println("Error: Unrecognized symbols at top of input file."); // maybe change this to a general error msg later? or write this in output file?
+            return false;
+        }
+
+
+        // each table will include the non-blank trimmed lines between its parent keyword (exclusive) and next keyword (exclusive)
+
+        List<String> nameTable = allNonBlankTrimmedLines.subList(nameKeyIndex + 1, courseSlotsKeyIndex);
+        List<String> courseSlotsTable = allNonBlankTrimmedLines.subList(courseSlotsKeyIndex + 1, labSlotsKeyIndex);
+        List<String> labSlotsTable = allNonBlankTrimmedLines.subList(labSlotsKeyIndex + 1, coursesKeyIndex);
+        List<String> coursesTable = allNonBlankTrimmedLines.subList(coursesKeyIndex + 1, labsKeyIndex);
+        List<String> labsTable = allNonBlankTrimmedLines.subList(labsKeyIndex + 1, notCompatibleKeyIndex);
+        List<String> notCompatibleTable = allNonBlankTrimmedLines.subList(notCompatibleKeyIndex + 1, unwantedKeyIndex);
+        List<String> unwantedTable = allNonBlankTrimmedLines.subList(unwantedKeyIndex + 1, preferencesKeyIndex);
+        List<String> preferencesTable = allNonBlankTrimmedLines.subList(preferencesKeyIndex + 1, pairKeyIndex);
+        List<String> pairTable = allNonBlankTrimmedLines.subList(pairKeyIndex + 1, partialAssignmentsKeyIndex);
+        List<String> partialAssignmentsTable = allNonBlankTrimmedLines.subList(partialAssignmentsKeyIndex + 1, allNonBlankTrimmedLines.size()); // make sure to test when this keyword is last line in file. (it should be fine)
+
+
+        // 4. parse each of the 10 subarrays in order, storing the data into data structure fields of this class.
+
+        // NOTE: must define data structures before doing this
+
+        if (!setNameData(nameTable)) {
+            System.out.println("Error: Invalid name data");
+            return false;
+        }
+
+        if (!setCourseSlotsData(courseSlotsTable)) {
+            System.out.println("Error: Invalid course slots data");
+            return false;
+        }
+
+        if (!setLabSlotsData(labSlotsTable)) {
+            System.out.println("Error: Invalid lab slots data");
+            return false;
+        }
+
+        if (!setCoursesData(coursesTable)) {
+            System.out.println("Error: Invalid courses data");
+            return false;
+        }
+
+        if (!setLabsData(labsTable)) {
+            System.out.println("Error: Invalid labs data");
+            return false;
+        }
+
+        if (!setNotCompatibleData(notCompatibleTable)) {
+            System.out.println("Error: Invalid not compatible data");
+            return false;
+        }
+
+        if (!setUnwantedData(unwantedTable)) {
+            System.out.println("Error: Invalid unwanted data");
+            return false;
+        }
+
+        if (!setPreferencesData(preferencesTable)) {
+            System.out.println("Error: Invalid preferences data");
+            return false;
+        }
+
+        if (!setPairData(pairTable)) {
+            System.out.println("Error: Invalid pair data");
+            return false;
+        }
+
+        if (!setPartialAssignmentsData(partialAssignmentsTable)) {
+            System.out.println("Error: Invalid partial assignments data");
+            return false;
+        }
+
+
+
+        // get here if no errors occurred...
+
+        return true; // default return
+    }	
+    
+
+
+
+    // TODO
+
+
+    // return True if no error occurred...
+    private boolean setNameData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setCourseSlotsData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setLabSlotsData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setCoursesData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setLabsData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setNotCompatibleData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setUnwantedData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setPreferencesData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setPairData(List<String> table) {
+
+        return true;
+    }
+
+    // return True if no error occurred...
+    private boolean setPartialAssignmentsData(List<String> table) {
+
+        return true;
+    }
+
 
 
 }
